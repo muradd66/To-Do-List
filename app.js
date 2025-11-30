1946bf3f039b1cb7bf3620b5b76ad9116cb78a64
muradd66 <muradmmdzade2006@gmail.com>
2025-12-01 00:05:46 +0400

    sort

--- app.js
+++ app.js
@@ -9,17 +9,18 @@ let sortUpb = document.querySelector(".sort-up-black")
 let list = document.querySelector(".list")
 let inputContainer = document.querySelector(".input-container")
 let plus = document.querySelector(".plus")
+let sorts=document.querySelector(".sorts")
 
 //x icon
 
 iconXPurple.addEventListener("mouseout", () => {
     iconXPurple.style.display = "none"
-    iconX.style.display = "inline-block"
+    iconX.style.display = "block"
 })
 
 iconX.addEventListener("mouseover", () => {
     iconX.style.display = "none"
-    iconXPurple.style.display = "inline-block"
+    iconXPurple.style.display = "block"
 })
 
 // sort icon
@@ -27,26 +28,26 @@ let sort = false
 
 sortDowng.addEventListener("mouseover", () => {
     sortDowng.style.display = "none"
-    sortDownb.style.display = "inline-block"
+    sortDownb.style.display = "block"
 })
 
 sortDownb.addEventListener("mouseout", () => {
     sortDownb.style.display = "none"
     if (sort == false) {
-        sortDowng.style.display = "inline-block"
+        sortDowng.style.display = "block"
     }
 
 })
 
 sortUpg.addEventListener("mouseover", () => {
     sortUpg.style.display = "none"
-    sortUpb.style.display = "inline-block"
+    sortUpb.style.display = "block"
 })
 
 sortUpb.addEventListener("mouseout", () => {
     sortUpb.style.display = "none"
     if (sort == true) {
-        sortUpg.style.display = "inline-block"
+        sortUpg.style.display = "block"
     }
 })
 
@@ -54,7 +55,7 @@ sortDownb.addEventListener("click", () => {
     sort = true
     sortDowng.style.display = "none"
     sortDownb.style.display = "none"
-    sortUpg.style.display = "inline-block"
+    sortUpg.style.display = "block"
     sortUpb.style.display = "none"
 })
 
@@ -62,7 +63,7 @@ sortUpb.addEventListener("click", () => {
     sort = false
     sortUpg.style.display = "none"
     sortUpb.style.display = "none"
-    sortDowng.style.display = "inline-block"
+    sortDowng.style.display = "block"
     sortDownb.style.display = "none"
 })
 
@@ -74,24 +75,25 @@ iconXPurple.addEventListener("click", () => {
     input.value = ''
 })
 
-
+let index=0
 add.addEventListener("click", () => {
-    let text = input.value
 
+    let text = input.value
     if (text != "") {
         inputContainer.style.display = "none"
+        sorts.style.display="block"
         input.value = ""
+
+        index=index+1
+
         let icon = document.createElement("img")
         icon.src = "./x-icon.svg"
         icon.style.display = "inline"
         icon.className = "delete"
 
-
-
         let iconPurple = document.createElement("img")
         iconPurple.src = "./x-icon-purple.svg"
         iconPurple.className = "delete-purple"
-
 
         let area = document.createElement("div")
         area.className = "area"
@@ -102,6 +104,12 @@ add.addEventListener("click", () => {
         p.textContent = text
         p.className = "content"
 
+        let number=document.createElement("span")
+        number.className="number"
+        number.textContent=`${index}. `
+
+        p.prepend(number)
+
         div.append(p)
         div.append(icon)
         div.append(iconPurple)
@@ -109,7 +117,6 @@ add.addEventListener("click", () => {
         list.append(area)
 
         list.style.display = "block"
-
 
         iconPurple.addEventListener("mouseout", () => {
             icon.style.display = "inline-block"
@@ -135,7 +142,6 @@ add.addEventListener("click", () => {
     }
 
 })
-
 
 plus.addEventListener("click", () => {
     inputContainer.style.display = "block"
